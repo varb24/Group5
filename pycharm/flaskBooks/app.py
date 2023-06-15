ef16550a7066a69185decc4546e9cf3d3a1a9e82
David Colina <Davidcolina11@hotmail.com>
2023-06-15 18:43:49 -0400

    Comments

--- pycharm/flaskBooks/app.py
+++ pycharm/flaskBooks/app.py
@@ -5,11 +5,12 @@ import helperFunctions
 app = Flask(__name__)
 
 
-
+# Define a route for getting data by user id
 @app.route('/rating/<int:userid>', methods=['GET'])
 def get_data_by_id(userid):
     helperFunctions.handle_get_rating_request(userid)
-    #
+    #Call the 'handle_get_rating_request' function from the 'helperFunctions'
+    #Handles the GET request for ratings by a user
 
 if __name__ == '__main__':
     app.run()
--- pycharm/flaskBooks/helperFunctions.py
+++ pycharm/flaskBooks/helperFunctions.py
@@ -1,19 +1,30 @@
 from flask import Flask, jsonify
 import pyodbc
+
+
+# Database connection params
 def get_db_connection():
     server = 'Hal'
     database = 'BooksAFewHundred'
     username = 'Group'
     password = 'book'
 
+    # Connecting to the database using PyODBC
     conn = pyodbc.connect(
         'DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
     return conn
+
+
+# Function that gets ratings by user
 def get_ratings_by_user(conn, userid):
+    # Create a cursor from the connection
     cursor = conn.cursor()
+    # Execute the SQL query
     cursor.execute('SELECT RatingId, Rating, BookId, UserId FROM Rating WHERE UserId = ?', userid)
+    # Fetch all rows from the executed SQL query
     rows = cursor.fetchall()
 
+    # Process rows into a list of dictionaries to be returned
     result = []
     for row in rows:
         result.append({
@@ -24,8 +35,12 @@ def get_ratings_by_user(conn, userid):
         })
     return result
 
+
 def handle_get_rating_request(userid):
+    # Database connection
     conn = get_db_connection()
+    # Retrieve the ratings for the specified user from database
     result = get_ratings_by_user(conn, userid)
+    # Close the database connection
     conn.close()
     return jsonify(result)
