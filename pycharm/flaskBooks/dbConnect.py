25706837b9bd152420394290bf8b770a31533194
varb24 <bahamut246@gmail.com>
2023-06-22 20:26:45 -0400

    Updated dbConnect and TestPost Api
    Configured file to work with patrcik DB and local server

--- pycharm/flaskBooks/dbConnect.py
+++ pycharm/flaskBooks/dbConnect.py
@@ -3,28 +3,41 @@ import pyodbc, requests
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+#Testing only
+def connectLocal():
+    connectInfo = ("Driver={SQL Server Native Client 11.0};"
+                   "Server=DESKTOP-I7PCMKQ;"
+                   "Database=BooksAFewHundred;"
+                   "Trusted_Connection=yes;")
 
-connectInfo = ("Driver={SQL Server Native Client 11.0};"
-               "Server=DESKTOP-I7PCMKQ;"
-               "Database=books;"
-               "Trusted_Connection=yes;")
+    connection = pyodbc.connect(connectInfo)
+    cursor = connection.cursor()
+    return cursor
+#Patrick server
+def get_db_connection():
+    server = 'Hal'
+    database = 'BooksAFewHundred'
+    username = 'Group'
+    password = 'book'
 
-connection = pyodbc.connect(connectInfo)
-cursor = connection.cursor()
-
-
+    # Connecting to the database using PyODBC
+    conn = pyodbc.connect(
+        'DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+    cursor = conn.cursor()
+    return cursor
+#Object pipes to DB
+cursor = get_db_connection()
 @app.route('/books/post/', methods=['POST'])
 def postBook():
-
     book = bookEntity.Book(ISBN=request.json['ISBN'], bookName=request.json['name'],
                            bookDescription=request.json['description'], price=request.json['price'],
-                           author=request.json['author'],
-                           genre=request.json['genre'], publisher=request.json['publisher'],
+                           author=request.json['authorID'],
+                           genre=request.json['genre'], publisher=request.json['publisherID'],
                            yearPublished=request.json['year'],
-                           copies_sold=request.json['copies_sold'])
+                           copies_sold=request.json['copiesSold'])
 
     # Will throw exception if there is duplicate data
-    dbBoilerPlate = "INSERT INTO [dbo].[books_data] ([ISBN],[Name],[description],[price],[genre],[author],[publisher],[year],[copies_Sold]) Values("
+    dbBoilerPlate = "INSERT INTO [dbo].[Book] ([ISBN],[Name],[description],[price],[genre],[authorID],[publisherID],[year],[copiesSold]) Values("
     # entry string is the entire command which tells the database to add information
     entryString = dbBoilerPlate + book.__str__() + ")"
     try:
@@ -35,6 +48,7 @@ def postBook():
         print("IntegrityError. Likely duplicate primary key value for book: " + book.__str__())
     return "PUT request successful!"
 
+
 @app.route('/books/<isbn>', methods=['GET'])
 def get_book(isbn):
     query = f"SELECT * FROM books_data WHERE isbn = '{isbn}'"
@@ -43,7 +57,8 @@ def get_book(isbn):
     if book:
         book_details = {
             'ISBN': book.ISBN, 'name': book.name, 'description': book.description, 'price': book.price,
-            'genre': book.genre, 'publisher': book.publisher, 'year': book.year, 'copies sold': book.copies_sold
+            'genre': book.genre, 'author': book.authorID, 'publisherID': book.publisherID, 'year': book.year,
+            'copies sold': book.copiesSold
         }
         return jsonify(book_details)
     else:
